3697aced596ad43b5643810c497429b006d1d041
swl <wanli.shao@daddylab.com>
2020-07-17 17:52:33 +0800

    完成

--- sign/views.py
+++ sign/views.py
@@ -63,3 +63,33 @@ def guest_manage(request):
 def sign_index(request, eid):
     event = get_object_or_404(Event, id=eid)
     return render(request, 'sign_index.html', {'event': event})
+
+#签到动作
+@login_required
+def sign_index_action(request, eid):
+    event = get_object_or_404(Event, id=eid)
+    phone = request.POST.get('phone', '')
+    print(phone)
+    result = Guest.objects.filter(phone=phone)
+    if not result:
+        return render(request, 'sign_index.html', {'event': event, 'hint': 'phone error.'})
+
+    result =Guest.objects.filter(phone=phone, event=eid)
+    if not result:
+        return render(request, 'sign_index.html', {'event': event, 'hint':'event id or phone error.'})
+
+    result = Guest.objects.get(phone=phone, event_id=eid)
+    if result.sign:
+        return render(request, 'sign_index.html', {'event': event, 'hint': 'user has sign in.'})
+    else:
+        Guest.objects.filter(phone=phone, event_id=eid).update(sign='1')
+        sign_num = Guest.objects.filter(sign='1').count()
+        guest_num = Guest.objects.count()
+        return render(request, 'sign_index.html', {'event': event, 'hint': 'sign in success.', 'guest': result,"sign_num": sign_num, "guest_num": guest_num})
+
+#退出登录
+@login_required
+def logout(request):
+    auth.logout(request)
+    response = HttpResponseRedirect('/index/')
+    return response
